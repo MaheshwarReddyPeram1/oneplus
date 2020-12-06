468fbd9f5f09b4b743b3a09b03ecea638db92623
maheswarreddy peram <38140817+MaheshwarReddyPeram1@users.noreply.github.com>
2020-12-06 19:25:29 +0530

    Update settings.py

--- onemore/settings.py
+++ onemore/settings.py
@@ -81,6 +81,9 @@ DATABASES = {
     'HOST': 'localhost'
     }
 }
+import dj_database_url
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
